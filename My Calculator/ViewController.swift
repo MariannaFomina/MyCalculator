f2be874085f81b7a9bfd2cff8e2987fb4d3b1e28
MariannaFomina <Fomina.MArianna.V@gmail.com>
2022-11-20 01:30:28 +0300

    Калькулятор с ошибками

--- My Calculator/ViewController.swift
+++ My Calculator/ViewController.swift
@@ -7,18 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController
+class ViewController: UIViewController {
 
     var operand = ""
     var numberOne = ""
     var numberTwo = ""
+ 
     
-        @IBOutlet weak var resultLabel: UILabel!
-    
-    /* при нажатии на цифры, значение будет передаваться в переменную numberOne и отображаться элементе resultLabel. Далее если будет нажата кнопка с операндом значение будет записываться в переменную operand, и пользователь может ввести второе число, которое в свою очередь будет записываться в переменную numberTwo*/
-    
-    @IBAction func InputNumber(_ sender: UIButton) {
-   
+    @IBOutlet weak var resultLabel: UILabel!
+
+
+/* Функция нажатия кнопок 0...9
+ При нажатии на цифры, значениебудет передаваться в переменную numberOne и отображаться элементе resultLabel. Далее если будет нажата кнопка с операндом значение будет записываться в переменную operand, и пользовательможет ввести второе число, которое в свою очередь будет записывться в переменную numberTwo
+ */
+    @IBAction func inputNumber(_ sender: UIButton) {
         if operand.isEmpty {
             numberOne = numberOne + (sender.titleLabel?.text)!
             resultLabel.text = numberOne
@@ -26,39 +28,40 @@ class ViewController: UIViewController
             numberTwo = numberTwo + (sender.titleLabel?.text)!
             resultLabel.text = numberTwo
         }
-        
-    //Функция обработки нажатия на клавиши +, -,  / , *
-    @IBAction func inputOperand(_ sender: UIButton) {
-        operand = sender.titleLabel?.text as! String }
-}
-    //Функция обнуления
-    @IBAction func clearAction(_ sender: UIButton) {
-        numberOne = ""
-            numberTwo = ""
-            operand = ""
-            resultLabel.text = "0"
     
+    // Фукция обнуления
+    @IBAction func clearAction(_ sender: UIButton) {
+  numberOne = ""
+  numberTwo = ""
+  operand = ""
+  resultLabel.text = "0"
+}
+      
+  // Функция обработки нажатия на клавиши +, -, /, *
+    @IBAction func inputOperand(_ sender: UIButton) {
+        operand = sender.titleLabel?.text as! String
     }
-    //кнопка результата
-    @IBAction func resultAction(_ sender: Any)
-        var result = 0.0
+  
+     //Кнопка результата
+        @IBAction func resultAction(_ sender: Any) {
+            var result = 0.0
+     
+            switch operand {
+            case "/":
+                result = Double(numberOne)! / Double(numberTwo)!
+            case "*":
+                result = Double(numberOne)! * Double(numberTwo)!
+            case "-":
+                result = Double(numberOne)! - Double(numberTwo)!
+            case "+":
+                result = Double(numberOne)! + Double(numberTwo)!
+            default:
+                break
+            }
         
-        switch operand {
-        case "/":
-            result = Double(numberOne)! / Double(numberTwo)!
-        case "*":
-            result = Double(numberOne)! * Double(numberTwo)!
-        case "-":
-            result = Double(numberOne)! - Double(numberTwo)!
-        case "+":
-            result = Double(numberOne)! + Double(numberTwo)!
-        default:
-            break
-        }
-        if result.truncatingRemainder(dividingBy: 1.0) == 00{
+        if result.truncatingRemainder(dividingBy:  1.0) == 00 {
             resultLabel.text = String(Int(result))
         } else {
             resultLabel.text = String(result)
         }
-    
-
+    }
